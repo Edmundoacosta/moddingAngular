65355f357beb422ef5f65ac0acb98ba5e6ef510f
Joel Infante <joel250598@gmail.com>
2019-07-05 20:02:39 -0500

    adding bootstrap to the proyect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,10 @@ import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-	{path: '', redirectTo: 'login', pathMatch: 'full'},
-	{path: 'login', component: LoginComponent},
-	{path: 'home', component: HomeComponent}
+	{path: '', redirectTo: 'home', pathMatch: 'full'},
+	{path: 'home', component: HomeComponent},
+	{path: 'login', component: LoginComponent}
+	
 ];
 
 @NgModule({
