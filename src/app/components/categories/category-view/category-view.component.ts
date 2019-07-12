8f3a9adfd5cf3bfe2f659093da5cf8e6372f078b
Edmundo <edmundoach@hotmail.com>
2019-07-11 23:27:53 -0500

    category-view, filter and view

--- src/app/components/categories/category-view/category-view.component.ts
+++ src/app/components/categories/category-view/category-view.component.ts
@@ -8,6 +8,92 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class CategoryViewComponent implements OnInit {
 	public category:string = '';
+  public products:Array<any> = [
+    {
+      name: 'Lorem Ipsum Dolor Sit.',
+      price: '11.00',
+      image: 'https://picsum.photos/300',
+      category: 'refrigeracion',
+    },
+    {
+      name: 'Lorem Ipsum Dolor Sit.',
+      price: '8.00',
+      image: 'https://picsum.photos/300',
+      category: 'refrigeracion',
+    },
+    {
+      name: 'Lorem Ipsum Dolor Sit.',
+      price: '11.00',
+      image: 'https://picsum.photos/300',
+      category: 'refrigeracion',
+    },
+    {
+      name: 'Lorem Ipsum Dolor Sit.',
+      price: '11.00',
+      image: 'https://picsum.photos/300',
+      category: 'refrigeracion',
+    },
+    {
+      name: 'Lorem Ipsum Dolor Sit.',
+      price: '10.00',
+      image: 'https://picsum.photos/300',
+      category: 'sistema',
+    },
+    {
+      name: 'Lorem Ipsum Dolor Sit.',
+      price: '10.00',
+      image: 'https://picsum.photos/300',
+      category: 'sistema',
+    },
+    {
+      name: 'Lorem Ipsum Dolor Sit.',
+      price: '10.00',
+      image: 'https://picsum.photos/300',
+      category: 'sistema',
+    },
+    {
+      name: 'Lorem Ipsum Dolor Sit.',
+      price: '10.00',
+      image: 'https://picsum.photos/300',
+      category: 'sistema',
+    },
+    {
+      name: 'Lorem Ipsum Dolor Sit.',
+      price: '10.00',
+      image: 'https://picsum.photos/300',
+      category: 'compu',
+    },
+    {
+      name: 'Lorem Ipsum Dolor Sit.',
+      price: '10.00',
+      image: 'https://picsum.photos/300',
+      category: 'compu',
+    },
+    {
+      name: 'Lorem Ipsum Dolor Sit.',
+      price: '10.00',
+      image: 'https://picsum.photos/300',
+      category: 'compu',
+    },
+    {
+      name: 'Lorem Ipsum Dolor Sit.',
+      price: '10.00',
+      image: 'https://picsum.photos/300',
+      category: 'compu',
+    },
+    {
+      name: 'Lorem Ipsum Dolor Sit.',
+      price: '10.00',
+      image: 'https://picsum.photos/300',
+      category: 'compu',
+    },
+    {
+      name: 'Lorem Ipsum Dolor Sit.',
+      price: '10.00',
+      image: 'https://picsum.photos/300',
+      category: 'compu',
+    }
+  ];
   constructor(
   	public activatedRoute: ActivatedRoute
   	) { }
@@ -17,6 +103,7 @@ export class CategoryViewComponent implements OnInit {
   		.subscribe(params => {
   			let name = params.type;
   			this.category = name.charAt(0).toUpperCase() + name.slice(1);
+        this.products = this.products.filter(prod => prod.category == params.type);
   			});
   		};
   }
