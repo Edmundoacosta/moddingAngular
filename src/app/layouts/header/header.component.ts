17fc6c5bba6d10a8760c3bc4e6cb0fcfeb68f38b
Joel Infante <joel250598@gmail.com>
2019-08-10 22:26:13 -0500

    linking login with the modal login on header

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+import { Location, LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { Router } from '@angular/router';
+import { SessionService } from '../../providers/session.service';
+import { ModdingService } from '../../providers/moddinpc.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  providers: [ Location, {provide: LocationStrategy, useClass: PathLocationStrategy},
+  	FormBuilder
+  ]
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  public userForm: FormGroup;
+
+  constructor(
+    public session: SessionService,
+    public _modService: ModdingService,
+    private _fb: FormBuilder,
+    private location: Location) { }
 
   ngOnInit() {
+  	this.userForm = this._fb.group({
+  		email: ['', Validators.required],
+  		password: ['', Validators.required]
+    })
+  }
+
+  onSubmit(user, valid){
+  	if (valid) {
+  		this._modService.login({user: user}).then((res) => {
+        window.location.reload();
+        console.log(this.session.getObject('user'));
+	  		console.log(res);
+	  	});
+  	}
   }
 
 }
