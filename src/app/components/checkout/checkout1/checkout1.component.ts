7e8e9e48f0bd7bc92699c218cdc2623b0541e099
Joel Infante <joel250598@gmail.com>
2019-09-30 01:16:44 -0500

    adding content to the checkout 1 and checkout 2

--- src/app/components/checkout/checkout1/checkout1.component.ts
+++ src/app/components/checkout/checkout1/checkout1.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModdingService } from '../../../providers/moddinpc.service';
 
 @Component({
   selector: 'app-checkout1',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout1.component.scss']
 })
 export class Checkout1Component implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+    public user = {
+      firstname: '',
+      lastname: '',
+      address1: '',
+      address2: '',
+      distric: '',
+      email: ''
+  
+    };
+    public allDepartments:Array<string> = ['Amazonas', 'Áncash', 'Apurímac', 'Arequipa', 'Ayacucho', 'Cajamarca', 'Callao', 'Cuzco', 'Huancavelica', 'Huánuco', 'Ica', 'Junín', 'La Libertad', 'Lambayeque', 'Lima', 'Loreto', 'Madre de Dios', 'Moquegua', 'Pasco', 'Piura', 'Puno', 'San Martín', 'Tacna', 'Tumbes', 'Ucayali'];
+    
+    public allDistrics:Array<string> =
+    [
+      'Ate','Barranco','Breña','Carabayllo','Chaclacayo','Chorillos','Cieneguilla','Comas','El Agustino','Independencia','Jesús María','La Molina','La Victoria','Lima','Lince','Los Olivos','Lurigancho-Chosica','Lurín','Magdalena del Mar','Magdalena Vieja','Miraflores','Pachacamac','Pucusana','Pueblo Libre','Puente Piedra','Punta Hermosa','Punta Negra','Rimac','San Juan de Miraflores','San Bartolo','San Borja','San Isidro','San Luis','San Martín de Porres','San Miguel','Santa Anita','Santa Maria del Mar','Santa Rosa','Santiago de Surco','Surquillo','Villa El Salvador','Villa María del Triunfo'
+    ];
+    constructor(
+      public _modService: ModdingService
+      ) { }
+  
+    ngOnInit() {
+      this._modService.getUser()
+        .then((res) =>{
+          this.user = res['user'];
+          console.log(this.user);
+        });
+    }
+  
   }
-
-}
