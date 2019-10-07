719e9516f77ec224b3421504ec80fb53af30fc00
Joel Infante <joel250598@gmail.com>
2019-10-06 21:10:38 -0500

    fixing checkout and buttons color

--- src/app/components/checkout/checkout1/checkout1.component.ts
+++ src/app/components/checkout/checkout1/checkout1.component.ts
@@ -7,31 +7,35 @@ import { ModdingService } from '../../../providers/moddinpc.service';
   styleUrls: ['./checkout1.component.scss']
 })
 export class Checkout1Component implements OnInit {
-    public user = {
-      firstname: '',
-      lastname: '',
-      address1: '',
-      address2: '',
-      distric: '',
-      email: ''
-  
-    };
-    public allDepartments:Array<string> = ['Amazonas', 'Áncash', 'Apurímac', 'Arequipa', 'Ayacucho', 'Cajamarca', 'Callao', 'Cuzco', 'Huancavelica', 'Huánuco', 'Ica', 'Junín', 'La Libertad', 'Lambayeque', 'Lima', 'Loreto', 'Madre de Dios', 'Moquegua', 'Pasco', 'Piura', 'Puno', 'San Martín', 'Tacna', 'Tumbes', 'Ucayali'];
-    
-    public allDistrics:Array<string> =
-    [
-      'Ate','Barranco','Breña','Carabayllo','Chaclacayo','Chorillos','Cieneguilla','Comas','El Agustino','Independencia','Jesús María','La Molina','La Victoria','Lima','Lince','Los Olivos','Lurigancho-Chosica','Lurín','Magdalena del Mar','Magdalena Vieja','Miraflores','Pachacamac','Pucusana','Pueblo Libre','Puente Piedra','Punta Hermosa','Punta Negra','Rimac','San Juan de Miraflores','San Bartolo','San Borja','San Isidro','San Luis','San Martín de Porres','San Miguel','Santa Anita','Santa Maria del Mar','Santa Rosa','Santiago de Surco','Surquillo','Villa El Salvador','Villa María del Triunfo'
-    ];
-    constructor(
-      public _modService: ModdingService
-      ) { }
-  
-    ngOnInit() {
-      this._modService.getUser()
-        .then((res) =>{
-          this.user = res['user'];
-          console.log(this.user);
-        });
-    }
-  
+  public user = {
+    firstname: '',
+    lastname: '',
+    email: '',
+    addressOne: ''
+  };
+
+  public deliveryAddress = {
+    name: '',
+    postalcode: null,
+    department: 'Lima',
+    district: ''
+  };
+	public allDepartments:Array<string> = ['Lima'];
+  public allDistricts:Array<string> = ['Ancon','Ate','Barranco','Breña','Carabayllo','Chaclacayo','Chorillos','Cieneguilla','Comas','El agustino','Indenpendencia','Jesus maria','La molina','La victoria','Lima', 'Lince', 'Los olivos', 'Lurigancho', 'Lurin', 'Magdalena del mar', 'Magdalena vieja', 'Miraflores', 'Pachacamac', 'Pucusana', 'Pueblo libre', 'Puente piedra', 'Punta negra', 'Punta hermosa', 'Rimac', 'S. juan de lurigancho', 'S. juan de miraflores', 'San bartolo', 'San borja', 'San isidro', 'San luis', 'San martin de porres', 'San miguel', 'Santa anita', 'Santa maria del mar', 'Santa rosa', 'Santiago de surco', 'Surquillo', 'Villa el salvador', 'Villa maria del triunfo'];
+
+  constructor(
+    public _modService: ModdingService
+    ) { }
+
+  ngOnInit() {
+  	this._modService.getUser()
+  		.then((res) =>{
+        this.user = res['user'];
+        this.user.addressOne = `${this.user['addresses'][0].name} - ${this.user['addresses'][0].district}`;
+        this.deliveryAddress.district = this.user['addresses'][0].district;
+        console.log(res);
+        console.log(this.deliveryAddress);
+  		});
+  }
+
   }
