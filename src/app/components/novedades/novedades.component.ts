45ce2265d3e0c8b6f671b6a8031eb6d19b7beaf9
Joel Infante <joel250598@gmail.com>
2019-12-05 11:43:24 -0500

    adding tabs navigation to the category child view

--- src/app/components/novedades/novedades.component.ts
+++ src/app/components/novedades/novedades.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-novedades',
   templateUrl: './novedades.component.html',
@@ -10,6 +11,27 @@ export class NovedadesComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    Instagram;
   }
 
+  callTest(){
+    test();
+  }
+  
 }
+function Instagram(Instafeed){
+  var userFeed = new Instafeed({
+    get: 'user',
+    userId: '12494311822',
+    limit: 12,
+    resolution: 'standard_resolution',
+    accessToken: '12494311822.1677ed0.569e11e0cf574263b8b0082163a5ce1b',
+    sortBy: 'most-recent',
+    template: '<img src="{{image}}" alt="{{caption}}" class="img-fluid"/>',
+  });
+  userFeed.run();
+}
+function test() {
+  alert("it works gaaaaaa!");
+}
+
