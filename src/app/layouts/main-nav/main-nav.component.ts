e74c29a76c0e37c948743d1e509fd959090224f7
Joel Infante <joel250598@gmail.com>
2019-12-05 16:51:14 -0500

    fixing cart

--- src/app/layouts/main-nav/main-nav.component.ts
+++ src/app/layouts/main-nav/main-nav.component.ts
@@ -69,6 +69,12 @@ export class MainNavComponent {
         }, 200);
     }
 
+    cart() {
+      this.router.navigate(['/shopping_cart']);
+      setTimeout(() => {
+        location.reload();
+      }, 200);
+    }
   
   }
 
