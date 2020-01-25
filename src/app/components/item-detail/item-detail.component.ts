34d4273deac2ab9f14ce7fb8675143384c3bc46a
Joel Infante <joel250598@gmail.com>
2020-01-25 17:53:34 -0500

    changin pdf url

--- src/app/components/item-detail/item-detail.component.ts
+++ src/app/components/item-detail/item-detail.component.ts
@@ -94,7 +94,7 @@ export class ItemDetailComponent implements OnInit {
   }
 
   openPdf(){
-    window.open('http://174.138.37.189:4444/public/pdfs/' + this.product['mainPdf'], "_blank");
+    window.open('https://ecogozotest.com:40/public/pdfs/' + this.product['mainPdf'], "_blank");
   }
   
   openGarantia() {
