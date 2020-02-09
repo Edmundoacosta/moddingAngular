defe79700f304b9cbaeb1a6b26bde9dd85c9d432
Joel Infante <joel250598@gmail.com>
2020-02-08 19:17:00 -0500

    changing home slider

--- src/app/providers/culqi.service.ts
+++ src/app/providers/culqi.service.ts
@@ -1,11 +1,7 @@
-import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 let Culqi: any;
 
-@Injectable({
-  providedIn: 'root'
-})
 export class CulqiService {
   settings: any = {
     title: '',
